0cb70bf3f8f45abc852855f38c0bb42a5c937e46
SamanthaMarston <116765591+SamanthaMarston@users.noreply.github.com>
2023-02-03 20:39:29 -0500

    Add files via upload

---
+++ cmpt220SamanthaMarstonA2.java
@@ -0,0 +1,51 @@
+cmpt220SamanthaMarston\
+    Module 02\
+        homework\
+            Problem1.java
+                import java.util.Scanner;
+                    {
+                    String name, Hometown;
+                    Scanner demo = new Scanner(System.in);
+                    //Input
+                    System.out.println("Enter your name: ");
+                    name = demo. nextLine();
+                    System.out.println();
+                    System.out.println("Enter your Hometown: ");
+                    Hometown = demo. next Line();
+                    System.out.println();
+                    // Return
+                    System.out.println("Your name is: " + name);
+                    System.out.println("Your Hometown is:" +Hometown);
+                    }
+
+            Problem2.java
+                class Logical {
+                    public static void main(String[] args)
+                    {
+                        // variables
+                        int a = 6, b = 5, c = 7, d = 9;
+                
+                        // Displaying a, b, c
+                        System.out.println("Var1 = " + a);
+                        System.out.println("Var2 = " + b);
+                        System.out.println("Var3 = " + c);
+                        System.out.println("Var4 = " + d);
+                
+                        // using logical AND to verify
+                        // two constraints
+                        System.out.println("a + b = " + (a + b) );
+                        System.out.println("a - b = " + (a - b) );
+                        System.out.println("a * b = " + (a * b) );
+                        System.out.println("b / a = " + (b / a) );
+                        System.out.println("b % a = " + (b % a) );
+                        System.out.println("c % a = " + (c % a) );
+                        System.out.println("a++   = " +  (a++) );
+                        System.out.println("a-- = " + (a--) );
+
+                        // Check the difference in d++ and ++d
+                        System.out.println("d++   = " +  (d++) );
+                        System.out.println("++d   = " +  (++d) )
+                                            }
+                                            
+                                        
+                                    }
